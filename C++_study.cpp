029a4058b15e045e4c1faf9680214c7bafc481f3
jjonpark <rupinjs@gmail.com>
2022-06-12 21:48:44 +0900

    220612

--- C++_study.cpp
+++ C++_study.cpp
@@ -277,22 +277,269 @@
 //     printf("틀렸습니다!\n");
 // }
 
-
 // 배열
 
+// #include <stdio.h>
+
+// int main(void)
+// {
+//     //여러개의 변수를 함께, 동시에 생성
+//     int subway_array[3];
+//     subway_array[0]=30;
+//     subway_array[1]=40;
+//     subway_array[2]=50;
+
+//     for (int i = 0; i<3; i++)
+//     {
+//         printf("지하철 %d호차에 %d 명이 타고 있습니다. \n",i+1, subway_array[i]);
+//     }
+
+// ** 배열 생성시 주의 사항
+// 배열의 값은 초기화를 반드시 해야함
+// 배열 크기는 항상 상수로 선언
+// int arr[10] ={1,2} ; // 3번째 값부터는 자동으로 '0' 으로 초기화 된다.
+// float arr_f[5] = {1.0fm 2.0f, 3.0f};
+
+//문자 VS 문자열
+
+//문자열 끝에는 '끝'을 의미하는 NULL 문자 '\0'이 포함되어야 함
+//즉, 문자열 선언시 여유공간 1개 가 필요
+// char str[7] = "cooding";
+// }
+
+// #include <stdio.h>
+
+// int main(void)
+// {
+//     printf("%c\n", 'a');
+//     printf("%d\n", 'a');
+
+//     printf("%c\n", 'A');
+//     printf("%d\n", 'A');
+
+//     printf("%c\n", '1');
+//     printf("%d\n", '1');
+
+// //참고 2 : 0~127 사이의 아스키 코드 정수값에 해당하는 문자 확인
+// for(int i =0; i<128;i++)
+// {
+//     printf("아스키 코드 정수 %d : %c\n", i,i);
+// }
+// }
+// #include <stdlib.h>
+// #include <stdio.h>
+// #include <time.h>
+
+// int main(void)
+// {
+//     srand(time(NULL));
+//     printf("\n\n === 아빠는 대머리 게임 === \n\n");
+//     int answer;
+//     int treatment = rand()% 4;
+// }
+
+//포인터
+// #include <stdio.h>
+// void swap(int a, int b);
+// void swap_addr(int *a, int *b);
+// int main(void)
+// {
+// //포인터
+// //[철수] : 101호
+// //[영희] : 201호
+// //[민수] : 301호
+// int a = 1;
+// int b = 2;
+// int c = 3;
+
+// printf("철수네 주소 : %d, 암호 : %d\n", &a, a);
+// printf("영희네 주소 : %d, 암호 : %d\n", &b, b);
+// printf("민수네 주소 : %d, 암호 : %d\n", &c, c);
+
+// //미션맨의 등장
+
+// //첫번째 미션은 아파트의 각 집에 방문하여 문에 적힌 암호 확인
+// int * mission_man; //포인터 변수
+// mission_man = &a;
+// printf("미션맨이 방문하는 곳 주소 : %d, 암호 : %d\n", mission_man, *mission_man);
+
+// mission_man = &b;
+// printf("미션맨이 방문하는 곳 주소 : %d, 암호 : %d\n", mission_man, *mission_man);
+
+// mission_man = &c;
+// printf("미션맨이 방문하는 곳 주소 : %d, 암호 : %d\n", mission_man, *mission_man);
+
+// //두 번째 미션 : 각 암호에 3을 곱해라
+// mission_man = &a;
+// *mission_man = *mission_man*3;
+// printf("미션맨이 암호를 바꾼곳 주소 : %d, 암호 : %d\n", mission_man, *mission_man);
+
+// mission_man = &b;
+// *mission_man = *mission_man*3;
+// printf("미션맨이 암호를 바꾼곳 주소 : %d, 암호 : %d\n", mission_man, *mission_man);
+
+// mission_man = &c;
+// *mission_man = *mission_man*3;
+// printf("미션맨이 암호를 바꾼곳 주소 : %d, 암호 : %d\n", mission_man, *mission_man);
+
+// // 스파이
+// //미션맨이 바꾼 암호에서 2를 빼라
+// int *spy= mission_man;
+// printf("\n .... 스파이가 미션 수행하는 중 ....\n\n");
+// spy=&a;
+// *spy = *spy - 2;
+// printf("스파이가 방문하는 곳 주소 : %d, 암호 : %d\n", spy, *spy);
+
+// spy=&b;
+// *spy = *spy - 2;
+// printf("스파이가 방문하는 곳 주소 : %d, 암호 : %d\n", spy, *spy);
+
+// spy=&c;
+// *spy = *spy - 2;
+// printf("스파이가 방문하는 곳 주소 : %d, 암호 : %d\n", spy, *spy);
+
+// printf("\n ... 철수 영희 민수는 집에 오고서는 바뀐 암호를 보고 깜놀 ...\n\n");
+
+// printf("철수네 주소 : %d, 암호 : %d\n", &a, a);
+// printf("영희네 주소 : %d, 암호 : %d\n", &b, b);
+// printf("민수네 주소 : %d, 암호 : %d\n", &c, c);
+// //참고 미션맨이 사는곳의 주소도 있을것이다
+// printf("미션맨의 주소 : %d\n", &mission_man);
+// printf("스파이의 주소 : %d\n", &spy);
+
+// int arr[3] = {5, 10, 15};
+// int *ptr = arr;
+// for (int i = 0; i < 3; i++)
+// {
+//     printf("배열 arr[%d] 의 값 : %d\n", i, arr[i]);
+// }
+// for (int i = 0; i < 3; i++)
+// {
+//     printf("포인터 ptr[%d] 의 값 : %d\n", i, ptr[i]);
+// }
+
+// ptr[0] = 100;
+// ptr[1] = 200;
+// ptr[2] = 300;
+// for (int i = 0; i < 3; i++)
+// {
+//     printf("배열 arr[%d] 의 값 : %d\n", i, arr[i]);
+//     // arr[i]는 *(arr+i) 와 같은것이다.
+// }
+// for (int i = 0; i < 3; i++)
+// {
+//     printf("포인터 ptr[%d] 의 값 : %d\n", i, ptr[i]);
+// }
+// // arr = arr배열의 첫번째 값의 주소와 동일 == &arr[0]
+
+// Swap
+//     int a = 10;
+//     int b = 20;
+//     //a와 b 의 값을 바꾼다.
+//     printf("Swap 함수 전 => a : %d, b : %d\n", a,b);
+//     swap_addr(&a,&b);
+//     printf("Swap 함수 후 => a : %d, b : %d\n", a,b);
+
+//     return 0;
+// }
+
+// void swap(int a, int b)
+// {
+//     int temp = a;
+//     a=b;
+//     b=temp;
+//     printf("Swap 함수 내 => a : %d, b : %d\n", a,b);
+// }
+// void swap_addr(int *a, int *b)
+// {
+//     int temp = *a;
+//     *a=*b;
+//     *b=temp;
+//     printf("(주소값 전달)Swap 함수 내 => a : %d, b : %d\n", *a,*b);
+// }
+// void changeArray(int *ptr)
+
+// #include <stdio.h>
+// #include <time.h>
+//     //물고기가 6마리가 있어요 이들은 어항에 살고 있는데, 사막이에요  물이 다 증발하기 전에 부지런히 어항에 물을 줘서
+//     // 물고기를 살려주세요
+
+//     int level;
+//     int arrayFish[6];
+//     void initData();
+//     void printfFishes();
+//     int *cursor; // 1번 물고기한테 먹이를 줄지 2번 물고기 한테 먹이를 줄지 결정하는 변수
+//     int main(void)
+//     {
+//         long startTime = 0;        //게임 시작 시간
+//         long totalElapsedTime = 0; // 총 경과 시간
+//         long prevElapsedTime = 0;  // 직전 경과 시간 (최근에 물을 준 시간 간격)
+
+//         int num; //몇번 어항에 물을 줄것인지, 사용자 입력
+//         initData();
+
+//         cursor = arrayFish;
+
+//         startTime = clock(); // 현재 시간을 milisecond(1000분의 1초) 단위로 반환
+//         while (1)
+//         {
+//             printfFishes()
+//                 getchar()
+//         }
+//         return 0;
+//     }
+
+//     void initData()
+//     {
+//         level = 1; // 게임 레벨 (1~5)
+//         for (int i = 0; i < 6; i++)
+//         {
+//             arrayFish[i] = 100; //어항의 물 높이 (0~100)
+//         }
+//     }
+//     void printfFishes()
+//     {
+//         printf("%3d번 %3d번 %3d번 %3d번 %3d번 %3d번\n", 1, 2, 3, 4, 5, 6);
+//         for (int i = 0; i < 6; i++)
+//         {
+//             printf(" %4d ", arrayFish[i]);
+//         }
+//         printf("\n\n");
+//     };
+
+//다차원 배열
 #include <stdio.h>
 
 int main(void)
 {
-    //여러개의 변수를 함께, 동시에 생성
-    int subway_array[3];
-    subway_array[0]=30;
-    subway_array[1]=40;
-    subway_array[2]=50;
+    // int i;
+    // int arr[5];
+    // int arr2[2][5];
+    // //ㅁㅁㅁㅁㅁ
+    // //ㅁㅁㅁㅁㅁ
+    // int arr3[4][2];
+    // //ㅁㅁ
+    // //ㅁㅁ
+    // //ㅁㅁ
+    // //ㅁㅁ
 
-    for (int i = 0; i<3; i++)
+    int arr[5] = {1, 2, 3, 4, 5};
+    int arr2[2][5] = {
+        {1, 2, 3, 4, 5},
+        {1, 2, 3, 4, 5}
+        };
+    int arr3[4][2]={
+        {1,2},
+        {1,2},
+        {1,2},
+        {1,2}
+    };
+    for (int i=0; i<4 ; i++)
     {
-        printf("지하철 %d호차에 %d 명이 타고 있습니다. \n",i+1, subway_array[i]);
+        for (int j=0; j<2; j++)
+        {
+            printf("2차원배열 (%d, %d) 의 값 : %d\n",i ,j, arr3[i][j]);
+        }
     }
-
+    return 0;
 }

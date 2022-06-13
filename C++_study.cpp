88594967ae3c04329af61b91b0258496002de0b8
jjonpark <rupinjs@gmail.com>
2022-06-13 23:43:46 +0900

    220613

--- C++_study.cpp
+++ C++_study.cpp
@@ -508,38 +508,207 @@
 //     };
 
 //다차원 배열
+// #include <stdio.h>
+
+// int main(void)
+// {
+//     // int i;
+//     // int arr[5];
+//     // int arr2[2][5];
+//     // //ㅁㅁㅁㅁㅁ
+//     // //ㅁㅁㅁㅁㅁ
+//     // int arr3[4][2];
+//     // //ㅁㅁ
+//     // //ㅁㅁ
+//     // //ㅁㅁ
+//     // //ㅁㅁ
+
+//     int arr[5] = {1, 2, 3, 4, 5};
+//     int arr2[2][5] = {
+//         {1, 2, 3, 4, 5},
+//         {1, 2, 3, 4, 5}
+//         };
+//     int arr3[4][2]={
+//         {1,2},
+//         {1,2},
+//         {1,2},
+//         {1,2}
+//     };
+//     for (int i=0; i<4 ; i++)
+//     {
+//         for (int j=0; j<2; j++)
+//         {
+//             printf("2차원 배열 (%d, %d) 의 값 : %d\n",i ,j, arr3[i][j]);
+//         }
+//     }
+//     return 0;
+// }
+
+//프로젝트
+// #include <stdlib.h>
+// #include <stdio.h>
+// #include <time.h>
+// // 10마리의 서로 다른 동물(각 카드 2장씩) 사용자로부터 2개의 입력값을 받아서 -> 같은 동물 찾으면 카드 뒤집기
+// //  모든 동물 쌍을 찾으면 게임 종료
+// //  총 실패 횟수 알려주기
+
+// void initAnimalArray();
+// int arrayAnimal[4][5];
+// int checkAnimal[4][5]; //뒤집혔는지 여부 확인
+// char *strAnimal[10];
+// char *cPtr = "테스트";
+// int getEmptyPosition();
+// int conv_pos_x(int x);
+// int conv_pos_x(int x);
+// void printAnimals();
+// void printQuestion();
+
+// int main(void)
+// {
+//     srand(time(NULL));
+
+//     initAnimalArray();
+//     initAnimalName();
+//     shuffleAnimal();
+
+//     int failCount = 0; //실패 횟수를 저장
+
+//     while (1)
+//     {
+//         int select1 = 0; //사용자가 선택한 처음 수
+//         int select2 = 0; //사용자가 선택한 두번째 수
+
+//         printAnimals();  // 동물위치 출력
+//         printQuestion(); // 문제 출력 (카드 지도)
+//         printf("뒤집을 카드를 2개 고르세요 : ");
+//         scanf("%d %d", &select1, &select2);
+
+//         if (select1 == select2)
+//         {
+//             continue;
+//         }
+//         //좌표에 해당하는 카드를 뒤집어 보고 같은지 안같은지 확인
+//         int firstSelect_x = conv_pos_x(select1);
+//         int firstSelect_y = conv_pos_y(select1);
+
+//         int secondSelect_x = conv_pos_x(select2);
+//         int secondSelect_y = conv_pos_y(select2);
+
+//         if ((checkAnimal[firstSelect_x][firstSelect_y] == 0 && checkAnimal[secondSelect_x][secondSelect_y] == 0) &&
+//             (arrayAnimal[firstSelect_x][firstSelect_y] == arrayAnimal[secondSelect_x][secondSelect_y]))
+//         {
+//             printf("\n\n빙고! : %s 발견 \n\n", strAnimal[firstSelect_x][firstSelect_y]);
+//             checkAnimal[firstSelect_x][firstSelect_y] = 1;
+//             checkAnimal[secondSelect_x][secondSelect_y] = 1;
+//         }
+//     }
+
+//     return 0;
+// }
+
+// void initAnimalArray()
+// {
+//     for (int i = 0; i < 4; i++)
+//     {
+//         for (int j = 0; j < 5; j++)
+//         {
+//             arrayAnimal[i][j] = -1;
+//         }
+//     }
+// }
+
+// void initAnimalName()
+// {
+//     strAnimal[0] = "원숭이";
+//     strAnimal[1] = "하마";
+//     strAnimal[2] = "강아지";
+//     strAnimal[3] = "고양이";
+//     strAnimal[4] = "돼지";
+
+//     strAnimal[5] = "코끼리";
+//     strAnimal[6] = "기린";
+//     strAnimal[7] = "낙타";
+//     strAnimal[8] = "타조";
+//     strAnimal[9] = "호랑이";
+// }
+
+// void shuffleAnimal()
+// {
+//     for (int i = 0; i < 10; i++)
+//     {
+//         for (int j = 0; j < 2; j++)
+//         {
+//             int pos = getEmptyPosition();
+//             int x = conv_pos_x(pos);
+//             int y = conv_pos_y(pos);
+
+//             arrayAnimal[x][y] = i
+//         }
+//     }
+// }
+
+// //좌표에서 빈 공간 찾기
+// int getEmptyPosition()
+// {
+//     while (1)
+//     {
+//         int randPos = rand() % 20; // 0~19 사이의 숫자 반환
+
+//         int x = conv_pos_x(randPos);
+//         int y = conv_pos_y(randPos);
+
+//         if (arrayAnimal[x][y] == -1)
+//         {
+//             return randPos
+//         }
+//     }
+//     return 0;
+// }
+
+// int conv_pos_x(int x)
+// {
+//     return (x / 5);
+// }
+
+// int conv_pos_y(int y)
+// {
+//     return (y % 5);
+// }
+// void printAnimals() // 동물 위치 출력
+// {
+//     for (int i = 0; i < 4; i++)
+//     {
+//         for (int j = 0; j < 5; j++)
+//         {
+//             printf("%8s", strAnimal[arrayAnimal[i][j]]);
+//         }
+//         printf("\n");
+//     }
+//     printf("\n==============================\n\n")
+// }
+// void printQuestion()
+// {
+//     printf("\n\n(문제)\n");
+//     int seq = 0;
+//     for (int i = 0; i < 4; i++)
+//     {
+//         for (int j = 0; j < 5; j++)
+//         {
+//             if (checkAnimal[i][j] != 0)
+//             {
+//                 printf("%8s", strAnimal[arrayAnimal[i][j]]);
+//             }
+//             else
+//             {
+//                 printf("%d", seq);
+//             }
+//         }
+//     }
+// }
+
+// 구조체 : 여러 관련 있는 변수들을 묶어서 하나로 묶는 역할
+
 #include <stdio.h>
-
-int main(void)
-{
-    // int i;
-    // int arr[5];
-    // int arr2[2][5];
-    // //ㅁㅁㅁㅁㅁ
-    // //ㅁㅁㅁㅁㅁ
-    // int arr3[4][2];
-    // //ㅁㅁ
-    // //ㅁㅁ
-    // //ㅁㅁ
-    // //ㅁㅁ
-
-    int arr[5] = {1, 2, 3, 4, 5};
-    int arr2[2][5] = {
-        {1, 2, 3, 4, 5},
-        {1, 2, 3, 4, 5}
-        };
-    int arr3[4][2]={
-        {1,2},
-        {1,2},
-        {1,2},
-        {1,2}
-    };
-    for (int i=0; i<4 ; i++)
-    {
-        for (int j=0; j<2; j++)
-        {
-            printf("2차원배열 (%d, %d) 의 값 : %d\n",i ,j, arr3[i][j]);
-        }
-    }
-    return 0;
+int main(void){
+    printf("준성");
 }

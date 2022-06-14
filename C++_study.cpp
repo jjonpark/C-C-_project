20981c559d78c3b70e86a90877f071df1f862783
jjonpark <rupinjs@gmail.com>
2022-06-14 23:12:14 +0900

    220614

--- C++_study.cpp
+++ C++_study.cpp
@@ -708,7 +708,70 @@
 
 // 구조체 : 여러 관련 있는 변수들을 묶어서 하나로 묶는 역할
 
+// #include <stdio.h>
+// int main(void){
+//     printf("준성");
+// }
+
 #include <stdio.h>
-int main(void){
-    printf("준성");
+struct Gameinfo
+{
+    char *name;
+    int year;
+    int price;
+    char *company;
+};
+
+int main(void)
+{
+
+    //[게임 출시] 이름 : 나도게임 , 발매년도 : 2017년 가격 : 50원, 제작사 : 나도회사
+
+    char *name = "나도게임";
+    int year = 2017;
+    int price = 50;
+    char *company = "나도회사";
+
+    //[ 또 다른 게임 출시] 이름 : 너도게임 , 발매년도 : 2017년 가격 : 100원, 제작사 : 너도회사
+
+    // char *name = "너도게임";
+    // int year = 2017;
+    // int price = 100;
+    // char *company = "너도회사";
+
+    //구조체 사용
+    struct Gameinfo gameinfo1;
+    gameinfo1.name = "나도게임";
+    gameinfo1.year = 2017;
+    gameinfo1.price = 50;
+    gameinfo1.company = "나도회사";
+
+    //구조체 출력
+    printf("--게임 출시 정보 -- \n");
+    printf("게임명 :%s\n", gameinfo1.name);
+    printf("발매년도 :%d\n", gameinfo1.year);
+    printf("가격     :%d\n", gameinfo1.price);
+    printf("제작사   :%s\n", gameinfo1.company);
+
+    struct Gameinfo gameinfo2 = {"너도게임", 2017, 100, "너도회사"};
+    printf("--또 다른 게임 출시 정보 -- \n");
+    printf("게임명 :%s\n", gameinfo2.name);
+    printf("발매년도 :%d\n", gameinfo2.year);
+    printf("가격     :%d\n", gameinfo2.price);
+    printf("제작사   :%s\n", gameinfo2.company);
+
+    //구조체 배열
+    struct Gameinfo gameArray[2] = {
+        {"나도게임", 2017, 50, "나도회사"},
+        {"너도게임", 2017, 100, "너도회사"}};
+
+    //구조체 포인터
+    struct Gameinfo *gamePtr;
+    gamePtr = &gameinfo1;
+    printf("-- 미션맨의 게임 출시 정보 -- \n");
+    printf("게임명 :%s\n", (*gamePtr).name);
+    printf("발매년도 :%d\n", (*gamePtr).year);
+    printf("가격     :%d\n", (*gamePtr).price);
+    printf("제작사   :%s\n", (*gamePtr).company);
+    return 0;
 }
